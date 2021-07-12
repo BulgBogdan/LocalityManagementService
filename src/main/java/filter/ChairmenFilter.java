0092b079499ce463807a4aa1440172512f3a34cb
BulgBogdan <bulgakov.bogdan.0295@gmail.com>
2021-07-12 22:12:08 +0300

    added filters

---
+++ src/main/java/filter/ChairmenFilter.java
@@ -0,0 +1,58 @@
+package filter;
+
+import entity.User;
+import repository.UserDAOImpl;
+import service.UserDAO;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Objects;
+
+@WebFilter(urlPatterns = {"/create/infrastructure", "/create/locality", "/delete/infrastructure",
+        "/delete/locality", "/edit/infrastructure", "/edit/locality"})
+public class ChairmenFilter implements Filter {
+
+    private UserDAO userDAO = new UserDAOImpl(User.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession();
+        String userSessionName = (String) session.getAttribute("userSession");
+        User user = userDAO.getByUsername(userSessionName);
+        if ((Objects.nonNull(session)) && (session.getAttribute("userSession") != null)) {
+            if (isRoleUser(user)) {
+                chain.doFilter(request, response);
+            } else {
+                res.sendRedirect("/error");
+            }
+        } else {
+            res.sendRedirect("/login");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    private boolean isRoleUser(User user) {
+        boolean isUser;
+        if (user.getRole().getRole().equals("user")) {
+            isUser = false;
+        } else {
+            isUser = true;
+        }
+        return isUser;
+    }
+}
